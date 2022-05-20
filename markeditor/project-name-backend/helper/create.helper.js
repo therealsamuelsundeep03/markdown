33fd4f15ac7ac9415a2cba30118b3700f462ee47
therealsamuelsundeep03 <84494757+therealsamuelsundeep03@users.noreply.github.com>
2022-05-20 17:26:38 +0530

    Delete markeditor/project-name-backend directory

--- markeditor/project-name-backend/helper/create.helper.js
+++
@@ -1,9 +0,0 @@
-const mongo = require("../model/mongodb");
-
-const helper = {
-    createUser (username,email,password) {
-        return mongo.db.collection('auth').insertOne({username,email,password});
-    }
-}
-
-module.exports = helper;
--- markeditor/project-name-backend/helper/login.helper.js
+++
@@ -1,9 +0,0 @@
-const mongo = require("../model/mongodb");
-
-const helper = {
-    findUserByEmail(email){
-        return mongo.db.collection('auth').findOne({email});
-    }
-}
-
-module.exports = helper;
